c26774fb1ef12d5307067ab3ee9d380e2de2ade5
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-08-28 17:20:52 +0300

    Store the omniauth ID token for the sign out request

--- app/controllers/decidim/tunnistamo/sessions_controller.rb
+++ app/controllers/decidim/tunnistamo/sessions_controller.rb
@@ -28,8 +28,13 @@ module Decidim
       def end_user_session
         redirect_path = stored_location_for(resource || :user) || decidim.root_path
 
+        # The ID token hint needs to be preserved in the session in order for
+        # Tunnistamo to correctly remember where the user needs to be redirected
+        # to after a successful sign out.
+        id_token_hint = session['omniauth-tunnistamo.id_token']
         signed_out = (::Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
         store_location_for(:user, redirect_path)
+        session['omniauth-tunnistamo.id_token'] = id_token_hint
 
         signed_out
       end
