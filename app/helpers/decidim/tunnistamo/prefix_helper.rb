d568e473a5c27b54fe5cee65ce535058d7f2c087
Eero Lahdenperä <eero.lahdenpera@mainiotech.fi>
2021-09-28 18:32:40 +0300

    Cleaner prefix hash

--- app/helpers/decidim/tunnistamo/prefix_helper.rb
+++ app/helpers/decidim/tunnistamo/prefix_helper.rb
@@ -3,15 +3,34 @@
 module Decidim
   module Tunnistamo
     module PrefixHelper
-      def prefix_columns
-        @prefix_columns ||= begin
-          return 1 if current_organization.reference_prefix.blank?
+      def tunnistamo_prefix_hash
+        @tunnistamo_prefix_hash ||= { value: "#{current_organization.reference_prefix}-", small: prefix_columns_small, medium: prefix_columns_medium }
+      end
 
-          prefix_columns = current_organization.reference_prefix.length / 4
-          prefix_columns = 1 if prefix_columns.zero?
-          prefix_columns = 4 if prefix_columns > 4
-          prefix_columns
+      private
+
+      def prefix_columns_medium
+        @prefix_columns_medium ||= begin
+          prefix_columns_in_range(min: 1, max: 4, divider: 4)
         end
+      end
+
+      def prefix_columns_small
+        @prefix_columns_small ||= begin
+          prefix_columns_in_range(min: 2, max: 6, divider: 2)
+        end
+      end
+
+      def prefix_columns_in_range(min: 1, max: 4, divider: 4)
+        columns = reference_prefix_length / divider
+
+        columns = min if columns < min
+        columns = max if columns > max
+        columns
+      end
+
+      def reference_prefix_length
+        @reference_prefix_length ||= current_organization.reference_prefix.length
       end
     end
   end
