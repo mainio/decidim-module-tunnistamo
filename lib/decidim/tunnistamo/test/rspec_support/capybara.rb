a3224ae2a6a1a93b52bc76e9677afcaf841a911e
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2024-09-05 18:08:29 +0300

    Fix system spec issues with Chromedriver v127+
    See: https://github.com/decidim/decidim/pull/13282

--- lib/decidim/tunnistamo/test/rspec_support/capybara.rb
+++ lib/decidim/tunnistamo/test/rspec_support/capybara.rb
@@ -3,12 +3,17 @@
 # This re-registration is made because of problems with chromedriver v120.
 # Selenium methods are undefined without this change.
 # See: https://github.com/decidim/decidim/pull/12160
+#
+# Also fixes issue with chromedriver v127+ problem that freezes the system specs
+# due to the forced search engine window left on top of the screen.
+# See: https://github.com/decidim/decidim/pull/13282
 
 require "selenium-webdriver"
 
 Capybara.register_driver :headless_chrome do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
   options.args << "--headless=new"
+  options.args << "--disable-search-engine-choice-screen" # Prevents closing the window normally
   options.args << "--no-sandbox"
   options.args << if ENV["BIG_SCREEN_SIZE"].present?
                     "--window-size=1920,3000"
